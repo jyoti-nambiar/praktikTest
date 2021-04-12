d1d2ae314afa238cfdb04e7191d92d797c97f71d
Jyoti <jyoti.nambiar@medieinstitutet.se>
2021-04-12 17:36:11 +0200

    praktik test

---
+++ main.js
@@ -0,0 +1,11 @@
+(function cartFunction() {
+
+    const contactInfo = document.querySelector(".contact-info");
+
+    const mailBtn = document.querySelector(".mail-btn");
+    mailBtn.addEventListener("click", function () {
+        contactInfo.classList.toggle("show-cart");
+
+    });
+})();
+
