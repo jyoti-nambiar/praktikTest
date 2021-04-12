4a02f6b660360fb0843845115db6a9f96ba187dc
Jyoti <jyoti.nambiar@medieinstitutet.se>
2021-04-12 17:56:24 +0200

    function commit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-(function cartFunction() {
+(function displayFunction() {
 
     const contactInfo = document.querySelector(".contact-info");
 
     const mailBtn = document.querySelector(".mail-btn");
     mailBtn.addEventListener("click", function () {
-        contactInfo.classList.toggle("show-cart");
+        contactInfo.classList.toggle("show-div");
 
     });
 })();
